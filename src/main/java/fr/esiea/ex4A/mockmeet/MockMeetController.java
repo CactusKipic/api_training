be7c36cf073dc221fa5747b10d90ce9b8b055c12
CactusKipic <clement.azan@gmail.com>
2021-03-27 03:09:28 +0100

    Résolution des problèmes avec la correction par Korekto
    Modification de la requête /api/matches pour prendre en charge une requête sans les préférences de sexes comme appelé lors de la correction (résolution erreur 400)
    Création de deux nouvelles méthodes dérivé des méthodes déjà existantes dans ApiService afin de pouvoir obtenir des matchs sans préférences de sexes
    
    Résolution du coding style:
      - Passage d'apiService en final dans MockMeetController
      - Passage de agifyService en final dans ApiService
      - Réduction de la taille des méthodes à 10 lignes ou moins dans:
        - ApiService
        - UserStocks
      - Passage de UserStocks en non-static, et ajout d'une instance dans ApiService
    
    Modification et adaptation des tests pour tester les classes modifiés, méthodes modifiées et ajoutées.

--- src/main/java/fr/esiea/ex4A/mockmeet/MockMeetController.java
+++ src/main/java/fr/esiea/ex4A/mockmeet/MockMeetController.java
@@ -9,23 +9,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 public class MockMeetController {
     
     @Autowired
-    ApiService apiService;
+    final ApiService apiService;
+    
+    public MockMeetController(ApiService apiService) {
+        this.apiService = apiService;
+    }
     
     @PostMapping(path = "api/inscription")
     void registerUser(@RequestBody User user){
-        apiService.addNewUser(user);
+        try {
+            apiService.addNewUser(user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     @GetMapping(path = "api/matches", produces = MediaType.APPLICATION_JSON_VALUE)
     List<Match> getMatches(@RequestParam("userName") String name, @RequestParam("userCountry") String country,
                            @RequestParam("sex") String sexe,@RequestParam("sexPref") String sexePref){
-        return apiService.getMatchsFor(name, country, Sexes.valueOf(sexe), Sexes.valueOf(sexePref));
+        if(!apiService.userAlreadyRegistered(name, country))
+            return new MatchesResponse();
+        if(!sexe.equals("null") && !sexePref.equals("null"))
+            return apiService.getMatchsFor(name, country, Sexes.valueOf(sexe), Sexes.valueOf(sexePref));
+        else
+            return apiService.getMatchsFor(name, country);
     }
     
 }
--- src/test/java/fr/esiea/ex4A/mockmeet/tools/UserStocksTest.java
+++ src/test/java/fr/esiea/ex4A/mockmeet/tools/UserStocksTest.java
@@ -9,6 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserStocksTest {
     
+    UserStocks userStocks = new UserStocks();
+    
     @ParameterizedTest
     @CsvSource({
         "Brigitte,68,916,FR,brig@esiea.fr,BriTwtr,F,M",
@@ -17,19 +19,19 @@ class UserStocksTest {
     void userStocks_put_and_retrieve_test(String name, int age, int count, String country, String mail, String tweeter, String sex, String sexPref) {
         UserAgify userAgify = new UserAgify(name, age, count, country);
         User user = new User(mail, name, tweeter, country, Sexes.valueOf(sex), Sexes.valueOf(sexPref));
-        UserStocks.addUser(userAgify, user);
+        userStocks.addUser(userAgify, user);
         
-        assertEquals(UserStocks.getUserAgify(name, country), userAgify);
-        assertTrue(UserStocks.getAgifyUsersOfAge(age).contains(userAgify));
-        assertTrue(UserStocks.getUsersOfAgifyUser(userAgify).contains(user));
-        assertEquals(UserStocks.getUserAgify(user), userAgify);
+        assertEquals(userStocks.getUserAgify(name, country), userAgify);
+        assertTrue(userStocks.getAgifyUsersOfAge(age).contains(userAgify));
+        assertTrue(userStocks.getUsersOfAgifyUser(userAgify).contains(user));
+        assertEquals(userStocks.getUserAgify(user), userAgify);
         
-        assertTrue(UserStocks.getUsersOfAge(age).stream().reduce((u1, u2) -> {
+        assertTrue(userStocks.getUsersOfAge(age).stream().reduce((u1, u2) -> {
             u1.addAll(u2);
             return u1;
         }).get().contains(user));
         
-        assertTrue(UserStocks.userAlreadyExist(user));
-        assertFalse(UserStocks.userAlreadyExist(new User(mail, name+"Cpalui", tweeter, country, Sexes.valueOf(sex), Sexes.valueOf(sexPref))));
+        assertTrue(userStocks.userAlreadyExist(user));
+        assertFalse(userStocks.userAlreadyExist(new User(mail, name+"Cpalui", tweeter, country, Sexes.valueOf(sex), Sexes.valueOf(sexPref))));
     }
 }
